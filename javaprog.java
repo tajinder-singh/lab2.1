aa5c64ab5a3dc9aba3b5d670b8b29db14afd4502
Tajinder <tajinder.singh@outlook.in>
2021-02-24 20:13:15 +0530

    Update javaprog.java

--- javaprog.java
+++ javaprog.java
@@ -2,18 +2,8 @@ class javaprog{
 
 public static void main(static[] args){
 
-<<<<<<< HEAD
-System.out.prntln("Hello World!!!");
-=======
-System.out.prntln("Hello World !!..");
->>>>>>> cfd4ee10d10b4ffe9a3898f7dab256db9349ba8d
-
+  System.out.prntln("Hello World!!!");
 
 }
-
-
-
-
-
 }
 
